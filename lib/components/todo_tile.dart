c62e0bd2bf511270221943657fa78e26c42927af
herdeybayor <shereefdeen4real@gmail.com>
2024-11-01 16:23:59 +0100

    Refactor TodoTile to use StatelessWidget instead of StatefulWidget

--- lib/components/todo_tile.dart
+++ lib/components/todo_tile.dart
@@ -1,34 +1,30 @@
 import 'package:flutter/material.dart';
 
-class TodoTile extends StatefulWidget {
+class TodoTile extends StatelessWidget {
   final String taskName;
   final bool? taskCompleted;
   final Function(bool?)? onChanged;
 
-  const TodoTile({super.key, required this.taskName, required this.taskCompleted, required this.onChanged});
+  const TodoTile(
+      {super.key,
+      required this.taskName,
+      required this.taskCompleted,
+      required this.onChanged});
 
-  @override
-  State<TodoTile> createState() => _TodoTileState();
-}
-
-class _TodoTileState extends State<TodoTile> {
   @override
   Widget build(BuildContext context) {
     return Container(
-          padding: const EdgeInsets.all(24),
-          decoration: BoxDecoration(
-              color: Colors.yellow, borderRadius: BorderRadius.circular(12)),
-          child: Row(
-            children: [
-              Checkbox(
-                  value: widget.taskCompleted,
-                  onChanged: (value) {
-                    setState(() {
-                      widget.onChanged!(value);
-                    });
-                  }),
-              Text(widget.taskName),
-            ],
-          ));
+        padding: const EdgeInsets.all(24),
+        decoration: BoxDecoration(
+            color: Colors.yellow, borderRadius: BorderRadius.circular(12)),
+        child: Row(
+          children: [
+            Checkbox(
+                value: taskCompleted,
+                onChanged: onChanged,
+                activeColor: Colors.black),
+            Text(taskName),
+          ],
+        ));
   }
 }
